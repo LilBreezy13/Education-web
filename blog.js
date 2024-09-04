b1b6cd1125fd0b9e0e532dd3ef506ae12be65e11
G shine <nickyjay013@gmail.com>
2024-09-04 11:32:16 +0000

    API

---
+++ blog.js
@@ -0,0 +1,74 @@
+const API_KEY = "dc3ffa91c620416bb62c6bfe37dc6363";
+const url = "https://newsapi.org/v2/everything?";
+
+window.addEventListener("load", () => fetchNews("Ghana Education OR Pulse Ghana OR 3news"));
+
+async function fetchNews(query) {
+    const queryParams = new URLSearchParams({
+        q: query,
+        domains: '3news.com,pulse.com.gh',  // Restrict search to specific domains
+        apiKey: API_KEY,
+        language: 'en',
+        sortBy: 'publishedAt',  // Fetch the most recent articles
+        pageSize: 30,  // Limit to 20 articles
+    });
+
+    const res = await fetch(`${url}${queryParams}`);
+    const data = await res.json();
+    bindData(data.articles);
+}
+
+function bindData(articles) {
+    const cardsContainer = document.getElementById("cardscontainer");
+    const newsCardTemplate = document.getElementById("template-news-card");
+
+    cardsContainer.innerHTML = "";
+
+    // Display up to 20 articles
+    articles.slice(0, 40).forEach((article) => {
+        if (!article.urlToImage) return;
+
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+function fillDataInCard(cardClone, article) {
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = `${article.title.slice(0, 60)}...`;
+    newsDesc.innerHTML = `${article.description.slice(0, 150)}...`;
+
+    const date = new Date(article.publishedAt).toLocaleString("en-US", { timeZone: "Africa/Accra" });
+
+    newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    });
+}
+
+let curSelectedNav = null;
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = navItem;
+    curSelectedNav.classList.add("active");
+}
+
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+
+searchButton.addEventListener("click", () => {
+    const query = searchText.value;
+    if (!query) return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = null;
+});
